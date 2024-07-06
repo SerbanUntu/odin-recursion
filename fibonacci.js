e3b85e03e90d9462db8efdeca740c3c94a748fcc
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-07-06 11:08:01 +0300

    feat: recursive fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -19,3 +19,18 @@ console.log(fibs(2)) // [0, 1]
 console.log(fibs(3)) // [0, 1, 1]
 console.log(fibs(4)) // [0, 1, 1, 2]
 console.log(fibs(8)) // [0, 1, 1, 2, 3, 5, 8, 13]
+
+function fibsRec(n) {
+	if (n < 1) return []
+	if (n === 1) return [0]
+	if (n === 2) return [0, 1]
+	let prev = fibsRec(n - 1)
+	return prev.concat(prev[n - 2] + prev[n - 3])
+}
+
+console.log(fibsRec(0)) // []
+console.log(fibsRec(1)) // [0]
+console.log(fibsRec(2)) // [0, 1]
+console.log(fibsRec(3)) // [0, 1, 1]
+console.log(fibsRec(4)) // [0, 1, 1, 2]
+console.log(fibsRec(8)) // [0, 1, 1, 2, 3, 5, 8, 13]
